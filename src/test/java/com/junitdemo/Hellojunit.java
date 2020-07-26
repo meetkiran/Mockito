c1aee53c47e70b82aec14e4d2a12e1870904e670
abc <abc@example.com>
2020-07-26 14:53:21 +0530

    Branch test

---
+++ src/test/java/com/junitdemo/Hellojunit.java
@@ -0,0 +1,34 @@
+package com.junitdemo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class Hellojunit {
+
+	int ch, ch1;
+
+	public Hellojunit(int k, int j) {
+		ch = k;
+		ch1 = j;
+	}
+
+	@Parameters
+	public static Collection<int[]> paramTest() {
+		int[][] arr = { { 2, 2 }, { 3, 4 } };
+		return Arrays.asList(arr);
+	}
+
+	@Test
+	public void test() {
+		assertEquals(ch, 2);
+	}
+
+}
